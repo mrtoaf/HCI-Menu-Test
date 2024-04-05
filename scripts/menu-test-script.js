155dd8283cd11f5fdcd1a63044589a595e805af2
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2024-04-04 22:58:54 -0400

    fixed the menu thing again

--- scripts/menu-test-script.js
+++ scripts/menu-test-script.js
@@ -1,11 +1,13 @@
 // start the dropdown test
 document.getElementById('startDropdownTest').addEventListener('click', function() {
     window.location.href = './dropdown-test.html';
+    return false;
 });
 
 // start the popup test
 document.getElementById('startPopupTest').addEventListener('click', function() {
     window.location.href = './popup-test.html';
+    return false;
 });
 
 
