60022234ad2e3c52a0540d1089945563726aa2d6
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2024-04-04 23:04:11 -0400

    fixed back button

--- scripts/menu-test-script.js
+++ scripts/menu-test-script.js
@@ -1,13 +1,11 @@
 // start the dropdown test
 document.getElementById('startDropdownTest').addEventListener('click', function() {
     window.location.href = './dropdown-test.html';
-    return false;
 });
 
 // start the popup test
 document.getElementById('startPopupTest').addEventListener('click', function() {
     window.location.href = './popup-test.html';
-    return false;
 });
 
 
