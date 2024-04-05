9c278204743cff249e39fb71e59d24f539c29217
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2024-04-04 22:54:35 -0400

    fixedf menu button again

--- scripts/menu-test-script.js
+++ scripts/menu-test-script.js
@@ -1,11 +1,11 @@
 // start the dropdown test
 document.getElementById('startDropdownTest').addEventListener('click', function() {
-    window.location.href = 'dropdown-test.html';
+    window.location.href = './dropdown-test.html';
 });
 
 // start the popup test
 document.getElementById('startPopupTest').addEventListener('click', function() {
-    window.location.href = 'popup-test.html';
+    window.location.href = './popup-test.html';
 });
 
 
